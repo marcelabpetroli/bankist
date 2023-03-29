e4750b2559c732948bfd93148c64a0acd1ee4466
Marcela B Petroli <marcelabpetroli@gmail.com>
2023-03-29 16:23:22 +0200

    Refactor date creation logic

--- js/script.js
+++ js/script.js
@@ -13,9 +13,9 @@ const account1 = {
     '2020-01-28T09:15:04.904Z',
     '2020-04-01T10:17:24.185Z',
     '2020-05-08T14:11:59.604Z',
-    '2020-05-27T17:01:17.194Z',
-    '2020-07-11T23:36:17.929Z',
-    '2020-07-12T10:51:36.790Z',
+    '2023-03-27T17:01:17.194Z',
+    '2023-03-28T23:36:17.929Z',
+    '2023-03-29T10:51:36.790Z',
   ],
   currency: 'EUR',
   locale: 'pt-PT', // de-DE
@@ -80,6 +80,23 @@ const createUserName = (accs) => {
 };
 createUserName(accounts);
 
+const formatMovementDate = (date) => {
+  const calcDaysPassed = (date1, date2) => Math.round(Math.abs((date2 - date1) / (1000 * 60 * 60 * 24)));
+
+  const daysPassed = calcDaysPassed(new Date(), date);
+
+  if (daysPassed === 0) return 'Today';
+  if (daysPassed === 1) return 'Yesterday';
+  if (daysPassed <= 7) return `${daysPassed} days ago`;
+  else {
+    const day = `${date.getDate()}`.padStart(2, 0);
+    const month = `${date.getMonth() + 1}`.padStart(2, 0);
+    const year = date.getFullYear();
+
+    return `${day}/${month}/${year}`;
+  }
+};
+
 const displayMovements = (acc, sort = false) => {
   containerMovements.innerHTML = '';
 
@@ -90,11 +107,7 @@ const displayMovements = (acc, sort = false) => {
     const type = mov > 0 ? 'deposit' : 'withdrawal';
 
     const date = new Date(acc.movementsDates[i]);
-    const day = `${date.getDate()}`.padStart(2, 0);
-    const month = `${date.getMonth() + 1}`.padStart(2, 0);
-    const year = date.getFullYear();
-
-    const displayDate = `${day}/${month}/${year}`;
+    const displayDate = formatMovementDate(date);
 
     const html = `
     <div class="movements__row">
@@ -227,10 +240,6 @@ btnClose.addEventListener('click', (e) => {
   labelWelcome.textContent = `Log in to get started`;
   inputCloseUsername.value = inputClosePin.value = '';
   inputClosePin.blur();
-});
-
-btnSort.addEventListener('click', (e) => {
-  e.preventDefault();
 });
 
 let sorted = false;
